09fd53eb55ff17c26ce0a9a2077db952db004db6
chenlili <375587295@qq.com>
2018-11-29 14:41:27 +0800

    add step-03

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,50 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put, Param, Query, Body } from '@nestjs/common';
+
+// fake data
+const inLearningPlatforms = [
+  {
+    id: 1,
+    platformname: '极客教程',
+    url: 'https://www.geekjc.com',
+  },
+  {
+    id: 2,
+    platformname: 'geekjc',
+    url: 'https://www.geekjc.com',
+  },
+];
+
+export class PlatformDTO{
+  platformname: string;
+  url: string;
+}
 
 @Controller()
 export class AppController {
 
+  @Get(':platformId')
+  getPlatformById(@Param('platformId') id){
+    const platform = inLearningPlatforms.find(value => value.id === parseInt(id, 10)); // 解析后都是字串，要使用parseInt转成number
+    const resPlatform = new PlatformDTO();
+    resPlatform.platformname = platform.platformname;
+    resPlatform.url = platform.url;
+    return resPlatform;
+  }
+
   @Get()
-  list(): string {
-    return '极客教程';
+  queryedList(@Query() query): string {
+    return query;
   }
 
   @Post()
-  create(){
-    return '地址：https://www.geekjc.com';
+  create(@Body() platformDTO: PlatformDTO){ // platDTO: PlatformDTO代表platformDTO是PlatformDTO型別
+    return `平台:${platformDTO.platformname}已建立`;
   }
+
+  // @Post()
+  // create(){
+  //   return '地址：https://www.geekjc.com';
+  // }
 
   @Put()
   update(){
